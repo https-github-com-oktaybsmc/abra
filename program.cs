91d4d1f9f469cca5373265835425f1d712702b38
oktaybsmc <105498522+oktaybsmc@users.noreply.github.com>
2022-05-13 18:06:34 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace uygulama1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
